971be09cb503604804260394f77ad1d29cfb4cf9
George Crochiere <46457797+GeorgeCrochiere@users.noreply.github.com>
2020-12-18 13:15:36 -0500

    Updated SendCode.java to current version.

--- SendCode.java
+++ SendCode.java
@@ -6,25 +6,27 @@ import com.pi4j.io.gpio.*;
 
 public class SendCode {
 	
-	public void SendingLight(String inputString) throws InterruptedException {
+	AccurateTiming ac = new AccurateTiming();
+	
+	public void SendingLight(String inputString, double delay) throws InterruptedException {
 		final GpioController gpio = GpioFactory.getInstance();
 		final GpioPinDigitalOutput outPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_26, "MyLaser", PinState.LOW);
 		outPin.setShutdownOptions(true, PinState.LOW);
 		
-		int delay = 12;
-		
-		inputString = "11001011" + inputString;
+		inputString = inputString + "0000";
 		
 		for(int m = 0; m < inputString.length(); m++) {
 			char currentChar = inputString.charAt(m);
 			if (currentChar == '1') {
 				outPin.setState(PinState.HIGH);
 				outPin.high();
-				Thread.sleep(delay);
+				ac.delayTiming(delay);
+				//Thread.sleep(delay);
 			} else if (currentChar == '0') {
 				outPin.setState(PinState.LOW);
 				outPin.low();
-				Thread.sleep(delay);
+				ac.delayTiming(delay);
+				//Thread.sleep(delay);
 			}
 		}
 		gpio.shutdown();
