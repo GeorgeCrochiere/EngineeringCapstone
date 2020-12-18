bb1f7c176d4a357dcef9713750c5d64ae60cdb37
George Crochiere <46457797+GeorgeCrochiere@users.noreply.github.com>
2020-12-18 13:13:03 -0500

    Updated ConvertDecode.java to current version.

--- ConvertDecode.java
+++ ConvertDecode.java
@@ -5,18 +5,106 @@ public class ConvertDecode {
 	public String NumbersToLetters(String binaryInput) {
 		
 		String result = "";
+		String finalResult = "";
 		
-		for(int j = 0; j < binaryInput.length(); j+=8) {
-			String oneLetterBinary = binaryInput.substring(j, j+8);
-			int letter = Integer.parseInt(oneLetterBinary, 2);
+		for(int j = 0; j < binaryInput.length(); j++) {
+
+			String tempCharSet = binaryInput.substring(j, j+1);
+			while((binaryInput.substring(j+1, j+2)).equals(tempCharSet.substring(0,1))) {
+				tempCharSet = tempCharSet + binaryInput.substring(j+1, j+2);
+				j++;
+				if (tempCharSet.length() > 15) {
+					break;
+				}
+			}
 			
-			String[] letCode = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
-			int codeCorrelation = letter-97;
-			result = result + letCode[codeCorrelation];
+			if (tempCharSet.contains("1")) {
+				if (tempCharSet.length() < 5) {
+					result = result + ".";
+				} else {
+					result = result + "-";
+				}
+			} else if (tempCharSet.contains("0")) {
+				if (tempCharSet.length() > 4) {
+					result = result + " ";
+				} else if (tempCharSet.length() > 10) {
+					break;
+				}
+			}
 			
 		}
 		
-		return result;
+		System.out.println(result + " got here");
+		
+		for (int k = 0; k < result.length(); k++) {
+			String currentLetter = "";
+			
+			if ((result.substring(k, k+1)).equals(".") || (result.substring(k, k+1)).equals("-")) {
+				currentLetter = currentLetter + result.substring(k, k+1);
+			} else {
+				currentLetter = currentLetter + " ";
+				
+				if (currentLetter.equals(".- ")) {
+					finalResult = finalResult + "a";
+				}if (currentLetter.equals("-... ")) {
+					finalResult = finalResult + "b";
+				}if (currentLetter.equals("-.-. ")) {
+					finalResult = finalResult + "c";
+				}if (currentLetter.equals("-.. ")) {
+					finalResult = finalResult + "d";
+				}if (currentLetter.equals(". ")) {
+					finalResult = finalResult + "e";
+				}if (currentLetter.equals("..-. ")) {
+					finalResult = finalResult + "f";
+				}if (currentLetter.equals("--. ")) {
+					finalResult = finalResult + "g";
+				}if (currentLetter.equals(".... ")) {
+					finalResult = finalResult + "h";
+				}if (currentLetter.equals(".. ")) {
+					finalResult = finalResult + "i";
+				}if (currentLetter.equals(".--- ")) {
+					finalResult = finalResult + "j";
+				}if (currentLetter.equals("-.- ")) {
+					finalResult = finalResult + "k";
+				}if (currentLetter.equals(".-.. ")) {
+					finalResult = finalResult + "l";
+				}if (currentLetter.equals("-- ")) {
+					finalResult = finalResult + "m";
+				}if (currentLetter.equals("-. ")) {
+					finalResult = finalResult + "n";
+				}if (currentLetter.equals("--- ")) {
+					finalResult = finalResult + "o";
+				}if (currentLetter.equals(".--. ")) {
+					finalResult = finalResult + "p";
+				}if (currentLetter.equals("--.- ")) {
+					finalResult = finalResult + "q";
+				}if (currentLetter.equals(".-. ")) {
+					finalResult = finalResult + "r";
+				}if (currentLetter.equals("... ")) {
+					finalResult = finalResult + "s";
+				}if (currentLetter.equals("- ")) {
+					finalResult = finalResult + "t";
+				}if (currentLetter.equals("..- ")) {
+					finalResult = finalResult + "u";
+				}if (currentLetter.equals("...- ")) {
+					finalResult = finalResult + "v";
+				}if (currentLetter.equals(".-- ")) {
+					finalResult = finalResult + "w";
+				}if (currentLetter.equals("-..- ")) {
+					finalResult = finalResult + "x";
+				}if (currentLetter.equals("-.-- ")) {
+					finalResult = finalResult + "y";
+				}if (currentLetter.equals("--.. ")) {
+					finalResult = finalResult + "z";
+				}
+				
+				currentLetter = "";
+				
+			}
+			
+		}
+		
+		return finalResult;
 		
 	}
 	
