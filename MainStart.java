b14b34e6db003ce6b751c3793e58d21ac6785733
George Crochiere <46457797+GeorgeCrochiere@users.noreply.github.com>
2021-01-01 08:49:11 -0500

    Updated MainStart.java to fixed version

--- MainStart.java
+++ MainStart.java
@@ -12,28 +12,20 @@ public class MainStart {
 		RecieveCode rc = new RecieveCode();
 		SendCode sc = new SendCode();
 		
-		//Decoder1 d1 = new Decoder1();
-		//ConvertDecode cd = new ConvertDecode();
-		
 		Scanner scan = new Scanner(System.in);
 		
-		//System.out.println("Please input the rate at which pulses will be send (delay in ms): ");
-		double delayTime = 3;
+		double delayTime = 6;
 		
 		System.out.println("Please Select Mode: (Input or Output)");
 		String modeType = scan.nextLine();
 		String continuous = "Y";
-		
-		//System.out.println(ce.LettersToNumbers(e1.Encoding1("test")));
-		//System.out.println(d1.Decoding1(cd.NumbersToLetters("0111011101111010011011000110011101100011011001000111100001100001011001100110101001101010011110100111011001110101011001100111000101101001011101010110010001111001")));
 		
 		while(continuous.equals("Y") || continuous.equals("y")) {
 			if (modeType.equals("Output") || modeType.equals("output")) {
 				
 				System.out.println("What would you like to send");
 				String inputRawText = scan.nextLine();
-				//sc.SendingLight(ce.LettersToNumbers(e2.Encoding2(e1.Encoding1(inputRawText))));
-				sc.SendingLight(ce.LettersToNumbers(inputRawText), delayTime);
+				sc.SendingLight(ce.LettersToNumbers(e1.Encoding1(inputRawText)), delayTime);
 				
 				System.out.println("Would you like to send another message? [Y/N] ");
 				continuous = scan.nextLine();
